44f86712d5c1eec20b60e5c82352c5c8130aa404
shinjaewon99 <tlswodnjs99@naver.com>
2026-05-25 18:07:14 +0900

    feat: redis config 클래스 파일 추가

---
+++ src/main/java/com/side/room_project/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.side.room_project.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(String.format("redis://%s:%d", redisHost, redisPort));
+        return Redisson.create(config);
+    }
+}
